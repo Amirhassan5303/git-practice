323061183840d6883a6c6c41d51a026c2c4cd75f
Amirhassan Usefi <Amirhassan5303@gmail.com>
2023-01-04 15:54:42 +0100

    intentional changes

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-my_list = [1, 2, 3, 4, 5]
 my_list2 = ['Amirhassan', 'Ali', 'Amin', 'Taha']
 my_list3 = ['Abbas', 'Fatemeh']
+
+class Human:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
