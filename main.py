6720213decf01624cfb32d432583fb8a500d26e0
Amirhassan Usefi <Amirhassan5303@gmail.com>
2023-01-04 16:07:02 +0100

    add __str__ magic method to class

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ class Human:
     def __init__(self, fname, lname):
         self.fname = fname
         self.lname = lname
+
+    def __repr__(self):
+        return f'The person name is {self.fname}'
+         
