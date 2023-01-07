bc21d412cf7f8380a7644ee94b921956379e8ed4
Amirhassan Usefi <Amirhassan5303@gmail.com>
2023-01-07 14:07:39 +0100

    modify main.py file

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-my_list2 = ['Amirhassan', 'Ali', 'Amin', 'Taha']
-my_list3 = ['Abbas', 'Fatemeh']
-
-class Human:
-    def __init__(self, fname, lname):
-        self.fname = fname
-        self.lname = lname
-
-    def __repr__(self):
-        return f'The person name is {self.fname}'
-         
+print('This file is meant to track git performance')
