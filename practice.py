0807a54bf30a0699f3374cd36b7029299c665e74
Amirhassan Usefi <Amirhassan5303@gmail.com>
2023-01-09 17:34:49 +0100

    add Shape class to practice.py file

--- practice.py
+++ practice.py
@@ -1,1 +1,5 @@
-print("This file is created just for practice")
+from abc import ABC, abstractmethod
+class Shape(ABC):
+    def __init__(self, color='Green'):
+        print('Shape constructor called')
+        self.color = color
