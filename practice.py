e927ee416fb51df9ceab167c8602cac4bbbbeda3
Amirhassan Usefi <Amirhassan5303@gmail.com>
2023-02-19 13:50:28 +0100

    edit .gitignore and practice.py file

--- practice.py
+++ practice.py
@@ -1,5 +1,3 @@
-from abc import ABC, abstractmethod
-class Shape(ABC):
-    def __init__(self, color='Green'):
-        print('Shape constructor called')
-        self.color = color
+from random import randint
+
+random.randint(0, 100)
